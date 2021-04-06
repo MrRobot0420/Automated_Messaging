a940e258f8439235538144845a2c30f6323b4450
MrRobot0420 <faizanbaig0510@gmail.com>
2021-04-06 20:08:14 +0530

    First Commit

---
+++ Mini Project 1.py
@@ -0,0 +1,16 @@
+# Weighted exam score average
+
+# Enter how many exams done
+number_of_exams = int(input("Enter number of exams: "))
+
+# Enter how many these exams cover
+total_credits = int(input("Enter how many credits these exams cover: "))
+
+# Begin with average of 0 and then add up percentages from each exam
+average = 0
+for exam in range(number_of_exams):
+    score = int(input("Enter exam score: "))
+    exam_credits = int(input("Enter how many credits this exam covered: "))
+    average = average + score * exam_credits/total_credits
+print("Your average is", average)
+
